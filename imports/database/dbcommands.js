1ff8e810b6bbbfc38ac24f33594e4a714a30dfa6
James Davis <jedwarddavis902@gmail.com>
2016-09-20 17:03:58 -0400

    Added charts. Has error and is slow so I would like to find another implementation. Also added tables but the data is not reactive.

--- imports/database/dbcommands.js
+++ imports/database/dbcommands.js
@@ -5,3 +5,16 @@ export function getDistinct(collectionArray, column){
     });
     return _.pluck(disctinctArray,column);
 }
+
+export function getFullGasAmount(tankSize){
+    var pressure = 2100;
+
+    //gas constant and temperatures should stay the same
+    //convert psi to Pa and liters to m^3
+    var ret = (pressure*6895 * tankSize/1000)/(293.15 * 8.314);
+
+    //convery moles to liters
+    ret *= 22.4;
+
+    return ret;
+}
--- imports/pages/dashboard.js
+++ imports/pages/dashboard.js
@@ -1,11 +1,39 @@
 import { Template } from "meteor/templating";
 import { Meteor } from "meteor/meteor";
 import { DeviceData } from "../../imports/database/devicedata.js";
-import { getDistinct } from "../../imports/database/dbcommands.js";
+import { getDistinct, getFullGasAmount } from "../../imports/database/dbcommands.js";
+import { Tracker } from "meteor/tracker";
+import { ReactiveVar } from "meteor/reactive-var";
 import "./pageparts/toolbar.js";
 import "./dashboard.html";
 
 var limit = 20;
+var threshold = 0.2;
+var chartRenders = {};
+
+//suppresses debug from chartjs
+//console.log = function() {};
+
+function getDevicesFromLocation(location){
+    let array = DeviceData.find({Location : {$eq : location}}).fetch();
+    return getDistinct(array, "DeviceID");
+}
+
+function getTotals(location){
+    let devices = getDevicesFromLocation(location);
+    let full = 0;
+    let empty = 0;
+    for(x in devices){
+        let d = DeviceData.findOne({DeviceID : { $eq : devices[x] }}, { sort : {TimeStamp : -1}});
+        let t = getFullGasAmount(d.TankSize);
+        if(d.AmountRemaining/t >= threshold){
+            full++;
+        }else{
+            empty++;
+        }
+    }
+    return [full, empty];
+}
 
 function lastMonth(){
     let date = new Date();
@@ -13,6 +41,57 @@ function lastMonth(){
     date = date.getTime()/1000;
     return date;
 }
+
+
+Template.dashboard.onCreated(function dashCreated(){
+    this.locations = new ReactiveVar([]);
+
+    this.autorun(function(){
+        let locs = Template.instance().locations.get();
+        for(let x in locs){
+            if(chartRenders[locs[x]] !== true){
+                let ctx = document.getElementById("chart"+locs[x]);
+                if(ctx !== null || ctx !== undefined) {
+                    //sets width and height of the canvas
+                    let $canvas = $("#chart" + locs[x]);
+                    let $parent = $canvas.parent();
+                    $canvas.height($parent.height());
+
+                    let data = {
+                        labels: [
+                            "Full",
+                            "Empty"
+                        ],
+                        datasets: [
+                            {
+                                data: getTotals(locs[x]),
+                                backgroundColor: [
+                                    "#36A2EB",
+                                    "#FF6384"
+                                ],
+                                hoverBackgroundColor: [
+                                    "#36A2EB",
+                                    "#FF6384"
+                                ]
+                            }],
+                    };
+                    let myChart = new Chart(ctx, {
+                        type: 'doughnut',
+                        data: data,
+                        options: {
+                            maintainAspectRatio: false,
+                            legend: {
+                                display: false
+                            }
+                        }
+                    });
+                    chartRenders[locs[x]] = true;
+                }
+            }
+        }
+    });
+
+});
 
 Template.dashboard.helpers({
     user(){
@@ -23,10 +102,35 @@ Template.dashboard.helpers({
     },
     getLocations(){
         let array = DeviceData.find().fetch();
-        return getDistinct(array, "Location");
+        array = getDistinct(array, "Location");
+        Template.instance().locations.set(array);
+        return array;
     },
-    getDevicesFromLocation(location){
+    getChartName(location){
+        return "chart"+location;
+    },
+    getDivName(location){
+        return "div" + location;
+    },
+    getTanksTotals(){
         let array = DeviceData.find({Location : {$eq : location}}).fetch();
-        return getDistinct(array, "DeviceID");
+        array = getDistinct(array, "DeviceID");
+        let full = 0;
+        let empty = 0;
+        for(x in array){
+            let d = DeviceData.findOne({DeviceID : { $eq : array[x] }}, { sort : {TimeStamp : -1}});
+            let t = getFullGasAmount(d.TankSize);
+            if(d.AmountRemaining/t >= threshold){
+                full++;
+            }else{
+                empty++;
+            }
+        }
+        return [full, empty, full+empty];
     }
+
+});
+
+Template.dashboard.onDestroyed(function dashDestroyed(){
+    chartRenders = [];
 });
